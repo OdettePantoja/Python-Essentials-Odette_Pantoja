672ac69788044211c10e9e6260ba28bf48078fac
OdettePantoja <58484183+OdettePantoja@users.noreply.github.com>
2021-10-22 10:21:22 -0500

    Create unidad2.py

---
+++ unidad_2/unidad2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+type("texto1")
+print(type("texto1"))
+
+variable1=921
+variable2=89.2375
+Variable3=76-87j
+VaRiAbLE_4="CADENA DE TEXTO"
+variable5=4+90+56
+
+
+
+print(variable5-variable1)
