120ad53225030ad122f4267db7f949522881975a
OdettePantoja <58484183+OdettePantoja@users.noreply.github.com>
2021-10-22 22:26:33 -0500

    Add files via upload

---
+++ ejercicio1_parte_2odette_pantoja.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 21:29:59 2021
+
+@author: Odette
+"""
+
+print("Programa  que  identifica  el  tipo  de  dato  de  un  valor  ingresado  por  el  usuario,  se  realizarán  cinco interacciones: ")
+
+valor= eval(input("Primera Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+type(valor)
+
+valor= eval(input("Segunda Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Tercera Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Cuarta Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Quinta Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+
+print("¡YA NO SE HARÁN MÁS INTERACCIONES!")
