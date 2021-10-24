01d9ee906439f44992bbe0366107234c1a87594a
OdettePantoja <58484183+OdettePantoja@users.noreply.github.com>
2021-10-24 12:05:06 -0500

    Add files via upload

---
+++ ejercicio1/ejercicio1_parte_2odette_pantoja.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 21:29:59 2021
+
+@author: Odette
+"""
+
+print("Programa  que  identifica  el  tipo  de  dato  de  un  valor  ingresado  por  el  usuario,  se  realizarán  cinco interacciones: ")
+
+#1. SE GUARDA EN LA VARIABLE VALOR LA ENTRADA DE NÚMERO DADA POR EL USUARIO
+#CON LA FUNCIÓN EVAL SE DETECTA QUE TIPO DE NÚMERO ES EL INTRODUCIDO POR EL USUARIO
+#2.SE MUESTRA EL TIPO DE NÚMERO DADO POR EL USUARIO
+
+valor= eval(input("Primera Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+
+valor= eval(input("Segunda Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Tercera Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Cuarta Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+valor= eval(input("Quinta Interacción, ingrese un valor cualquiera: "))
+print("Este tipo de dato en Python es: ")
+print(type(valor)) 
+
+
+print("¡YA NO SE HARÁN MÁS INTERACCIONES!")
+
+#DESARROLLO DEL EJERCICIO EN LAS PLATAFORMAS:
+#GITHUB:
+#https://github.com/OdettePantoja/Python-Essentials-Odette_Pantoja.git
+#GITLAB:
+#https://gitlab.com/odettepantoja/curso-python.git
